6e67126cd7812a9b00d3ed5e754a34c3775cde8d
Jonas Boone <boone.jonas@outlook.be>
2023-02-13 12:23:51 +0100

    feat(exercise 7): solution

--- other/whats-a-mock/__tests__/thumb-war.3.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.3.todo.js
@@ -3,122 +3,17 @@ import thumbWar from '../thumb-war'
 import * as utils from '../utils'
 
 test('returns winner', () => {
-  // replace these lines with a call to jest.spyOn and
-  // call to mockImplementation on the mocked function (See hint #1)
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = (p1, p2) => p2
+  jest.spyOn(utils, 'getWinner')
+  // this allows us to overwrite the actual inner workings of the getWinner function and replace it 
+  // with our mocked logic
+  utils.getWinner.mockImplementation((_p1, p2) => p2)
 
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
   expect(winner).toBe('Kent C. Dodds')
+  expect(utils.getWinner.mock.calls).toEqual([
+    ['Ken Wheeler', 'Kent C. Dodds'],
+    ['Ken Wheeler', 'Kent C. Dodds']
+  ])
 
-  // replace the next two lines with a restoration of the original function
-  // (See hint #2)
-  utils.getWinner = originalGetWinner
+  utils.getWinner.mockRestore();
 })
-
-/*
-Hints below
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Hint #1:
-
-Here's an example of those APIs:
-
-const myObject = {foo: () => 'bar'}
-jest.spyOn(myObject, 'foo')
-myObject.foo.mockImplementation(() => 'not-bar')
-myObject.foo() === 'not-bar' // true
-
-
-See the solution file for the solution
-
-
-
-
-
-
-
-
-
-Hint #2:
-
-If we wanted to restore the mocked `myObject.foo` function
-to its original implementation, we could do:
-myObject.foo.mockRestore()
-
-And then the original implementation will be called.
-myObject.foo() === 'bar' // true
-
-
- */
