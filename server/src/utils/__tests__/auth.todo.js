7a0fb786db570409fd6d94ac0f720f8a0178c08e
Jonas Boone <boone.jonas@outlook.be>
2023-02-13 10:51:43 +0100

    refactor(exercise 4): alternative solution

--- server/src/utils/__tests__/auth.todo.js
+++ server/src/utils/__tests__/auth.todo.js
@@ -1,14 +1,17 @@
 import { userToJSON } from "../auth"
 
 test('userToJSON excludes secure properties', () => {
-    const user = {
+  const safeUser = {
     id: 'some-id',
     username: 'sarah',
+  }
+    const user = {
+    ...safeUser,
     exp: new Date(),
     iat: new Date(),
     hash: 'some really long string',
     salt: 'some shorter string',
   }
   const result = userToJSON(user)
-  expect(result).not.toContain(['exp', 'iat', 'hash', 'salt'])
+  expect(result).toEqual(safeUser)
 })
